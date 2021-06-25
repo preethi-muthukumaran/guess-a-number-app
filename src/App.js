f34ec5265b116473fe495e7880366fd2e085573d
preethi-muthukumaran <preethimksb@gmail.com>
2021-06-25 07:05:36 -0700

    Created (untested) ConfirmInputScreen.js as a screen to ask for confirmation for user input. Installed Navigation dependencies, added skeleton for navigation within App.js to navigate from StartGameScreen to ConfirmInputScreen (untested(.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './comp/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style = {styles.screen}>
+    <NavigationContainer>
       <Header title = "Guess a Number!"/>
-      <StartGameScreen />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Start"
+          components = {StartGameScreen}
+          options = {{title: 'Start'}}
+        />
+        <Stack.Screen
+          name = "Confirm Input"
+          components = {}
+          options = {{title: 'ConfirmInput'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/ConfirmInputScreen.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+import Card from '../comp/Card';
+import Colors from '../constants/colors';
+
+const ConfirmInputScreen = props => {
+    return(
+        <View style = {styles.main}>
+            <Card style = {styles.confirmContainer}>
+                <Text>Are you sure this is the number you want to enter?</Text>
+                <Text>{props.input}</Text>
+                <View style = {styles.buttonContainer}>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "Yes"
+                            onPress = {() => {}} //navigate on to next game screen
+                            color = {Colors.confirm}
+                        />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "No"
+                            onPress = {() => {}} //navigate back to start screen with empty field
+                            color = {Colors.reject}
+                        />
+                    </View>
+                </View>
+            </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    confirmContainer: {
+        width: 300,
+        maxWidth: '90%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10
+    },
+    button: {
+        width: 10
+    }
+});
+
+export default ConfirmInputScreen;
